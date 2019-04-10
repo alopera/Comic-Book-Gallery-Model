aa9de659db3f4f57e95164663a63065f2ab8fa1d
Alejandro lopera sanchez <alelopera@hotmail.com>
2019-04-10 15:07:26 -0500

    Added the initial project with the initial entity framework configuration and the first code workflow.

---
+++ ComicBookGalleryModel/Models/ComicBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel.Models
+{
+    public class ComicBook
+    {
+        // you can use ID, ComicBookId or ComicBookID to name the primaryKey of a table
+        public int Id { get; set; }
+        public Series Series{ get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public decimal Rating { get; set; }
+
+    }
+}
---
+++ ComicBookGalleryModel/Program.cs
@@ -0,0 +1,41 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace ComicBookGalleryModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                context.ComicBooks.Add(new ComicBook() {
+                    Description ="description test",
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today,
+                    Series = new Series() {
+                        Title = "The amazing Spider-man"
+                    }
+                });
+
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks
+                    .Include(cb => cb.Series)
+                    .ToList();
+                foreach (var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.Series.Title);
+                }
+
+                Console.ReadLine();
+            } 
+        }
+    }
+}
